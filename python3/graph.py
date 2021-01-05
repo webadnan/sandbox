2f6b64dbe799b102f53790480ebd24b70fbaf2fa
SM Adnan <webadnan@gmail.com>
2021-01-05 15:10:17 -0800

    Adding some python specific library code.

---
+++ python3/graph.py
@@ -0,0 +1,15 @@
+class BfsWithEdgeMatrix:
+  def __init__(self):
+    pass
+
+class BfsWithEdgeList:
+  def __init__(self):
+    pass
+
+class DfsWithEdgeMatrix:
+  def __init__(self):
+    pass
+
+class DfsWithEdgeList:
+  def __init__(self):
+    pass
