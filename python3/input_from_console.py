90f56069eb37193230aae3175b52a65a69020307
SM Adnan <webadnan@gmail.com>
2021-01-06 00:09:42 -0800

    Python console input is now splitting line with regular expression with any number of space or tabs.

--- python3/input_from_console.py
+++ python3/input_from_console.py
@@ -3,18 +3,21 @@
 #   tokens = Tokens()
 #   n = tokens.int()
 #   s = tokens.read()
+import re
 class Tokens:
     def __init__(self, splitter=' '):
         self.queue = []
         self.splitter = splitter
+    def readline(self):
+        while True:
+            line = input()
+            if line: break
+        return line
     def read(self):
         if len(self.queue) > 0:
             return self.queue.pop(0)
-        while True:
-            line = input()
-            if not line: continue
-            self.queue = line.split(self.splitter)
-            if len(self.queue) == 0: continue
-            return self.queue.pop(0)
+        line = self.readline()
+        self.queue = re.split('[\t ]', line)
+        return self.queue.pop(0)
     def int(self):
         return int(self.read())
